938f2d00a09dde2093672733a329f3609acebcf6
roziuk_5 <roziukp@gmail.com>
2018-03-27 21:47:57 +0300

    Command line parsing

--- parsing.py
+++ parsing.py
@@ -4,19 +4,42 @@ import sys
 
 def createParser():
 	parser=argparse.ArgumentParser()
-	parser.add_argument('-c',type=int,default=1)
-	parser.add_argument('name')
+	subparser=parser.add_subparsers (dest='command')
+
+	hello_parser=subparser.add_parser('hello')
+	hello_parser.add_argument('--name','-n', nargs='+', default=['world'])
+
+
+	goodbay_parser=subparser.add_parser('goodbay')
+	goodbay_parser.add_argument('-c',type=int, default=1)
 
 	return parser
+
+
+def run_hello(namespace):
+
+	for names in namespace.name:
+		print('Hello,{}'.format(names))
+
+
+def run_goodbay(namespace):
+	for _ in range(namespace.c):
+		print("Goodbay")
+
+
 
 def main():
 	parser=createParser()
 	namespace=parser.parse_args(sys.argv[1:])
 
 
-	for _ in range(namespace.c):
-		print('Hello, {}'.format(namespace.name))
+	if namespace.command == 'hello':
+		run_hello(namespace)
+	elif namespace.command == 'goodbay':
+		run_goodbay(namespace)
 
+	else:
+		print('Wrong input values!')
 
 if __name__=='__main__':
 	main()
